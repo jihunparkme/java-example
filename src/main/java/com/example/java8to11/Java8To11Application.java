5f56d52a0f084d2a4414b2019fa457b361b480e5
Jihun Park <jihunpark.tech@gmail.com>
2023-10-10 10:37:21 +0900

    [The Java] add: application java file

---
+++ src/main/java/com/example/java8to11/Java8To11Application.java
@@ -0,0 +1,11 @@
+package com.example.java8to11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8To11Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Java8To11Application.class, args);
+    }
+}
